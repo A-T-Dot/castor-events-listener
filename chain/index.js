bd40780554f732dfe8cd58f03b38fb10acbbdd1f
jim <isdotjim@gmail.com>
2019-09-16 10:20:28 +0800

    change substrate api types

--- chain/index.js
+++ chain/index.js
@@ -3,53 +3,71 @@ const { Keyring } = require('@polkadot/keyring');
 const { stringToU8a } = require('@polkadot/util');
 
 let api = null;
-let tcrApi = null;
-
-// https://github.com/substrate-developer-hub/substrate-tcr
-// https://github.com/substrate-developer-hub/substrate-tcr-ui/blob/master/src/services/tcrService.js
 
 const init = async (wsp, callback, subscribe, WS) => {
   const provider = new WsProvider(wsp);
-  api = await ApiPromise.create(provider);
 
-  api.rpc.system.chain();
-  api.rpc.chain.subscribeNewHeads((header) => {
-    subscribe(header, WS);
-  });
-
-  tcrApi = await ApiPromise.create({
+  api = await ApiPromise.create({
     types: {
-      Listing: {
-        id: 'u32',
-        data: 'Vec<u8>',
-        deposit: 'Balance',
-        owner: 'AccountId',
-        application_expiry: 'Moment',
-        whitelisted: 'bool',
-        challenge_id: 'u32'
+      ContentHash: "[u8; 32]",
+      NodeType: "u32",
+      Node: {
+        id: "ContentHash",
+        node_type: "NodeType",
+        sources: "Vec<ContentHash>"
+      },
+      GeId: "u64",
+      ActionId: "u64",
+      TcxId: "u64",
+      GovernanceEntity: {
+        threshold: "u64",
+        min_deposit: "Balance",
+        apply_stage_len: "Moment",
+        commit_stage_len: "Moment"
       },
       Challenge: {
-        listing_hash: 'Hash',
-        deposit: 'Balance',
-        owner: 'AccountId',
-        voting_ends: 'Moment',
-        resolved: 'bool',
-        reward_pool: 'Balance',
-        total_tokens: 'Balance'
+        amount: "Balance",
+        voting_ends: "Moment",
+        resolved: "bool",
+        reward_pool: "Balance",
+        total_tokens: "Balance",
+        owner: "AccountId"
+      },
+      ListingId: "u64",
+      ChallengeId: "u64",
+      Listing: {
+        id: "ListingId",
+        node_id: "ContentHash",
+        amount: "Balance",
+        application_expiry: "Moment",
+        whitelisted: "bool",
+        challenge_id: "ChallengeId",
+        owner: "AccountId"
       },
       Poll: {
-        listing_hash: 'Hash',
-        votes_for: 'Balance',
-        votes_against: 'Balance',
-        passed: 'bool'
+        votes_for: "Balance",
+        votes_against: "Balance",
+        passed: "bool"
       },
-      Vote: {
-        value: 'bool',
-        deposit: 'Balance',
-        claimed: 'bool'
+      Tcx: {
+        tcx_type: "u64"
       },
-      TokenBalance: 'u128'
-    }
+      TcxType: "u64",
+      Link: {
+        source: "u32",
+        target: "u32"
+      },
+      VecContentHash: "Vec<ContentHash>",
+      // server diff from fe
+      DispatchError: "u64", 
+      SetId: "u64"
+    },
+    provider
+  });
+
+  console.log(api.rpc.system.chain())
+  api.rpc.chain.subscribeNewHeads((header) => {
+    subscribe(header, WS);
   });
 
   callback(api);
@@ -69,23 +87,6 @@ const getBalances = async (addresses, callback) => {
   callback(balancesMap);
 };
 
-const transfer = async (addressFrom, addressTo, amount) => {
-  const fromPair = keyring.getPair(addressFrom);
-  api.tx.balances
-    .transfer(addressTo, amount)
-    .signAndSend(fromPair, ({ status }) => {
-      if (status.isFinalized) {
-        setStatus(`Completed at block hash #${status.asFinalized.toString()}`);
-      } else {
-        setStatus(`Current transfer status: ${status.type}`);
-      }
-    })
-    .catch(e => {
-      setStatus(':( transaction failed');
-      console.error('ERROR:', e);
-    });
-};
-
 const getKeysFromSeed = _seed => {
   if (!_seed) {
     throw new Error('Seed not valid.');
@@ -95,43 +96,6 @@ const getKeysFromSeed = _seed => {
   const paddedSeed = _seed.padEnd(32);
   return keyring.addFromSeed(stringToU8a(paddedSeed));
 };
-
-async function _createApiWithTypes () {
-  return await ApiPromise.create({
-    types: {
-      Listing: {
-        id: 'u32',
-        data: 'Vec<u8>',
-        deposit: 'Balance',
-        owner: 'AccountId',
-        application_expiry: 'Moment',
-        whitelisted: 'bool',
-        challenge_id: 'u32'
-      },
-      Challenge: {
-        listing_hash: 'Hash',
-        deposit: 'Balance',
-        owner: 'AccountId',
-        voting_ends: 'Moment',
-        resolved: 'bool',
-        reward_pool: 'Balance',
-        total_tokens: 'Balance'
-      },
-      Poll: {
-        listing_hash: 'Hash',
-        votes_for: 'Balance',
-        votes_against: 'Balance',
-        passed: 'bool'
-      },
-      Vote: {
-        value: 'bool',
-        deposit: 'Balance',
-        claimed: 'bool'
-      },
-      TokenBalance: 'u128'
-    }
-  });
-}
 
 // terminal console:
 
@@ -145,7 +109,6 @@ module.exports = {
   init,
   getBalance,
   getBalances,
-  // transfer,
   getKeysFromSeed
 }
 
